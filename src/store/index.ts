3283a2d975df45706b9f3b407437881cf7083190
ex_zhangwenlei@exiot.cmcc <15196725410@139.com>
2024-01-10 01:04:51 +0800

    feat(too): to

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
-import { useUserStore } from './user';
 import {usePersonConfig} from './personConfig';
 import { usePrizeConfig } from './prizeConfig';
 import {useGlobalConfig} from './globalConfig';
 export default function useStore() {
   return {
-    user: useUserStore(),
     personConfig:usePersonConfig(),
     prizeConfig:usePrizeConfig(),
     globalConfig:useGlobalConfig()
