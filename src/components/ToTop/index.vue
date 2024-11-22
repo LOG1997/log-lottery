fa3dcaefbf2707d91612d655968e867f227cf75c
ex_zhangwenlei@exiot.cmcc <15196725410@139.com>
2024-11-22 17:04:56 +0800

    fix: replace the toTop icon

--- src/components/ToTop/index.vue
+++ src/components/ToTop/index.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="fixed z-50 flex items-center justify-center w-10 h-10 rounded-full shadow-lg cursor-pointer right-12 bottom-12 bg-slate-700 hover:bg-slate-600">
-    <svg-icon name="totop" />
+    <svg-icon name="toTop" />
   </div>
 </template>
 
