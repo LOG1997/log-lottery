173dd796a40c613f2ed73c1d6720ee860b08f008
log1997 <2694233102@qq.com>
2025-10-03 11:39:20 +0800

    feat: :sparkles: 修复问题

---
+++ src/views/Config/Person/PersonAll/index.vue
@@ -0,0 +1,73 @@
+<!-- eslint-disable vue/no-parsing-error -->
+<script setup lang='ts'>
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import DaiysuiTable from '@/components/DaiysuiTable/index.vue'
+import CustomDialog from '@/components/Dialog/index.vue'
+import { useViewModel } from './useViewModel'
+
+const { resetData, deleteAll, handleFileChange, exportData, alreadyPersonList, allPersonList, tableColumns } = useViewModel()
+const { t } = useI18n()
+const limitType = '.xlsx,.xls'
+
+const resetDataDialogRef = ref()
+const delAllDataDialogRef = ref()
+</script>
+
+<template>
+  <CustomDialog
+    ref="resetDataDialogRef"
+    :title="t('dialog.titleTip')"
+    :desc="t('dialog.dialogResetWinner')"
+    :submit-func="resetData"
+  />
+  <CustomDialog
+    ref="delAllDataDialogRef"
+    :title="t('dialog.titleTip')"
+    :desc="t('dialog.dialogDelAllPerson')"
+    :submit-func="deleteAll"
+  />
+
+  <div class="min-w-1000px">
+    <h2>{{ t('viewTitle.personManagement') }}</h2>
+    <div class="flex gap-3">
+      <button class="btn btn-error btn-sm" @click="delAllDataDialogRef.showDialog()">
+        {{ t('button.allDelete') }}
+      </button>
+      <div class="tooltip tooltip-bottom" :data-tip="t('tooltip.downloadTemplateTip')">
+        <a
+          class="no-underline btn btn-secondary btn-sm" :download="t('data.xlsxName')" target="_blank"
+          :href="`/log-lottery/${t('data.xlsxName')}`"
+        >{{ t('button.downloadTemplate') }}</a>
+      </div>
+      <div class="">
+        <label for="explore">
+
+          <div class="tooltip tooltip-bottom" :data-tip="t('tooltip.uploadExcelTip')">
+            <input
+              id="explore" type="file" class="" style="display: none" :accept="limitType"
+              @change="handleFileChange"
+            >
+
+            <span class="btn btn-primary btn-sm">{{ t('button.importData') }}</span>
+          </div>
+        </label>
+      </div>
+      <button class="btn btn-error btn-sm" @click="resetDataDialogRef.showDialog()">
+        {{ t('button.resetData') }}
+      </button>
+      <button class="btn btn-accent btn-sm" @click="exportData">
+        {{ t('button.exportResult') }}
+      </button>
+      <div>
+        <span>{{ t('table.luckyPeopleNumber') }}:</span>
+        <span>{{ alreadyPersonList.length }}</span>
+        <span>&nbsp;/&nbsp;</span>
+        <span>{{ allPersonList.length }}</span>
+      </div>
+    </div>
+    <DaiysuiTable :table-columns="tableColumns" :data="allPersonList" />
+  </div>
+</template>
+
+<style lang='scss' scoped></style>
