bd97ab40d1b40dcd40fba1a39de2e4d552dd21be
LOG1997 <LOG1997@users.noreply.github.com>
2026-01-04 13:18:11 +0000

    deploy: ac18ba72fa8927e61ca65a9d44893d6bd9d322b9

---
+++ js/chunk-B4sTuCix.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-MRN7_AUH.js";import{K as o,o as s,J as t}from"./chunk-BB7fNWQZ.js";const n=r({},[["render",function(r,n){const a=t("router-view");return s(),o(a)}]]);export{n as default};
