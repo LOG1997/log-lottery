14a05ecec494e4956d9efcf67a64048d59e0914b
xiaoshi-jiang <82742793+xiaoshi-jiang@users.noreply.github.com>
2026-01-27 14:34:26 +0800

    Update defaultTitle for the lottery event

--- src/locales/modules/data.ts
+++ src/locales/modules/data.ts
@@ -31,7 +31,7 @@ export const dataZhCn = {
     operation: '操作',
     delete: '删除',
     removePerson: '移入未中奖名单',
-    defaultTitle: '大明内阁六部御前奏对',
+    defaultTitle: '海德曼年会👉幸运大抽奖',
     xlsxName: '人口登记表-zhCn.xlsx',
     readmeName: 'readme-zhCn.md',
 }
