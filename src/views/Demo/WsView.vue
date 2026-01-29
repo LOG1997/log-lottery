785c1df228bded9dbb3cb65883131e0a1bd2df84
log1997 <2694233102@qq.com>
2026-01-29 17:35:53 +0800

    feat: ✨ ws_server代码处理

---
+++ src/views/Demo/WsView.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import { useWebsocket } from '@/hooks/useWebsocket'
+import { button } from '../../locales/modules/button'
+
+const { open, close, send, status, data } = useWebsocket()
+const url = 'http://127.0.0.1:8080/echo?userSignature=123456&apiKey=api_key_mao'
+</script>
+
+<template>
+  <div>
+    <button class="btn" @click="open(url)">
+      连接
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ ws_server/src/api/user.rs
@@ -0,0 +1,40 @@
+use crate::structure::app::{ApiResponse, AppState};
+use actix_web::{HttpRequest, HttpResponse, Responder, post, web};
+
+impl<T> ApiResponse<T> {}
+#[post("/user-msg")]
+pub async fn user_msg(
+    req: HttpRequest,
+    req_body: String,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    println!("All headers:");
+    // 获取usersignature参数
+    let target_user_signature = req
+        .headers()
+        .get("userSignature")
+        .unwrap()
+        .to_str()
+        .unwrap();
+    // 打印接收到的消息
+    println!("Received /user-msg: {}", req_body);
+    // 获取对应 user_signature 的发送端
+    let tx_map = data.tx_map.read().await;
+    if let Some(tx) = tx_map.get(target_user_signature) {
+        match tx.send(req_body.clone()) {
+            Ok(_) => HttpResponse::Ok().json(ApiResponse::<()>::success_without_data(
+                "发送成功".to_string(),
+            )),
+            Err(e) => {
+                eprintln!("Failed to send message: {}", e);
+                HttpResponse::InternalServerError()
+                    .json(ApiResponse::<()>::error(500, "系统服务异常".to_string()))
+            }
+        }
+    } else {
+        HttpResponse::NotFound().json(ApiResponse::<()>::error(
+            404,
+            "后台服务未启动，请联系管理员".to_string(),
+        ))
+    }
+}
